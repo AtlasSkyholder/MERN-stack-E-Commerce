92966335e81ca69047fbc3b3c02904e212572a40
morbi <the_morbidus@hotmail.com>
2020-05-26 00:42:16 -0400

    react-dropzone

--- client/src/components/utils/FileUpload.js
+++ client/src/components/utils/FileUpload.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 function FileUpload() {
   return (
-    <div>
+    <div style={{ display:'flex', justifyContent:'space-between' }} >
+      FileUpload
+
+
+
       
     </div>
   )
