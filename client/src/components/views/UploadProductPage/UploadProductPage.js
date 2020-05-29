c0ef5afd919a60a0c42d4bddacdc07ba2aa54bb7
morbi <the_morbidus@hotmail.com>
2020-05-29 02:51:41 -0400

    submit form functionality for product creation in client and server complete

--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import { Typography. Button, Form, message, Input, Icon } from 'antd';
 import FileUpload from '../../utils/FileUpload';
+import Axios from 'axios';
 
 const { Title } = Typography;
 const { TextArea } = Input;
@@ -15,7 +16,7 @@ const Continents = [
   {key: 7, value: "Antartica"}
 ]
 
-function UploadProductPage() {
+function UploadProductPage(props) {
 
   const [TitleValue, setTitleValue] = useState("");
   const [DescriptionValue, setDescriptionValue] = useState("");
@@ -44,13 +45,36 @@ function UploadProductPage() {
     setImages(newImages);
   }
 
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    const variables = {
+      writer: props.user.userData._id,
+      title: TitleValue,
+      description: DescriptionValue,
+      price: PriceValue,
+      images: Images,
+      continents: ContinentValue
+
+    }
+
+    Axios.post('/api/product/uploadProduct', variables).then(response => {
+      if(response.data.success) {
+        alert('Product Successfully Uploaded');
+        props.history.push('/');
+      } else {
+        alert('Failed to upload Product');
+      }
+    })
+  }
+
   return (
     <div style={{ maxWidth:'700px', margin:'2rem auto' }}>
       <div style={{ textAlign:'center', marginBottom:'2rem'}}>
         <Title level={2}> Upload Travel Product</Title>
       </div>
       
-      <Form onSubmit >
+      <Form onSubmit={onSubmit} >
 
         {/* DropZone */}
         <FileUpload refreshFunction={updateImages} />
@@ -87,7 +111,7 @@ function UploadProductPage() {
         <br />
         <br />
 
-        <Button>
+        <Button onClick={onSubmit}>
           Submit
         </Button>
 
