a5d52755f8886d8fba285f5cf75388b663f45b8c
morbi <the_morbidus@hotmail.com>
2020-05-30 01:41:18 -0400

    Landing Page rendering part 2

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import Axios from 'axios';
-import { Icon } from 'antd';
+import { Icon, Col, Card, Row } from 'antd';
+
+const { Meta } = Card;
 
 function LandingPage() {
 
@@ -16,6 +18,22 @@ function LandingPage() {
         }
     })
   }, []);
+
+  const renderCards = Products.map((product, index)=> {
+      return (
+        <Col lg={6} md={8} xs={24}>
+          <Card 
+            hoverable={true}
+            cover={<ImageSlider images={product.images} />}
+            >
+            <Meta 
+              title={product.title}
+              description={`$${product.price}`}
+            />
+          </Card>
+        </Col>
+      )
+  })
 
   return (
     <div>
@@ -35,7 +53,9 @@ function LandingPage() {
         <h2>No post yet...</h2>
       </div>  :
       <div>
-            
+        <Row gutter={[16,16]}>
+          {renderCards}
+        </Row>
       </div>
         
       }
