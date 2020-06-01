cf248a0abd9c5b0ae60bd63869e20fc027684d85
morbi <the_morbidus@hotmail.com>
2020-05-31 23:57:52 -0400

    CheckBox rendering and functionality part 1

--- client/src/components/views/LandingPage/Sections/CheckBox.js
+++ client/src/components/views/LandingPage/Sections/CheckBox.js
@@ -1,8 +1,56 @@
-import React from 'react'
+import React from 'react';
+import { Checkbox, Collapse } from 'antd';
+
+const { Panel } = Collapse;
+
+const continents = [
+  {
+    "_id": 1,
+    "name": "Africa"
+  },
+  {
+    "_id": 2,
+    "name": "Europe"
+  },
+  {
+    "_id": 3,
+    "name": "Asia"
+  },
+  {
+    "_id": 4,
+    "name": "North America"
+  },
+  {
+    "_id": 5,
+    "name": "South America"
+  },
+  {
+    "_id": 6,
+    "name": "Australia"
+  },
+  {
+    "_id": 7,
+    "name": "Antartica"
+  }
+]
 
 function CheckBox() {
   return (
     <div>
+      <Collapse defaultActiveKey={['0']}>
+        <Panel header key="1">
+          {continents.map((value, index) => (
+            <React.Fragment key={index}>
+              <Checkbox
+                onChange
+                type="checkbox"
+                checked
+              />
+            </React.Fragment>
+          ))}
+
+        </Panel>
+      </Collapse>
       
     </div>
   )
