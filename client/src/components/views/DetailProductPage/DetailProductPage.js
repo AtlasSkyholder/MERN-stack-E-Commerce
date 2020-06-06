1839682d9c648e2b19844d426a50a6d7e591c75c
morbi <the_morbidus@hotmail.com>
2020-06-05 23:19:09 -0400

    Product ID rendering functionality part 4

--- client/src/components/views/DetailProductPage/DetailProductPage.js
+++ client/src/components/views/DetailProductPage/DetailProductPage.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Axios from 'axios';
+import ProductImage from './Sections/ProductImage';
+import ProductInfo from './Sections/ProductInfo';
 
 function DetailProductPage(props) {
 
@@ -14,10 +16,23 @@ function DetailProductPage(props) {
   }, [])
 
   return (
-    <div>
-      DetailProductPage
+    <div className="postPage" style={{ width: '100%', padding: '3rem 4rem' }}>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <h1>product Title</h1>
+      </div>
+
+      <br />
+
+      <Row gutter={[16, 16]} >
+        <Col lg={12} xs={24}>
+          <ProductImage />
+        </Col>
+        <Col lg={12} xs={24}>
+          <ProductInfo />
+        </Col>
+      </Row>
     </div>
   )
 }
 
-export default DetailProductPage
+export default DetailProductPage;
