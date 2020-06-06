7981936677aad5e67cbf2aaeee4210bbada94955
morbi <the_morbidus@hotmail.com>
2020-06-05 22:33:31 -0400

    added react-image-gallery package

--- client/src/components/views/DetailProductPage/DetailProductPage.js
+++ client/src/components/views/DetailProductPage/DetailProductPage.js
@@ -1,14 +1,15 @@
-import React, { useEffect } from 'react'
-import Axios from 'axios'
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
 
 function DetailProductPage(props) {
 
   const productId = props.match.params.productId;
+  const [Product, setProduct] = useState([]);
 
   useEffect(() => {
     Axios.get(`/api/product/products_by_id?id=${productId}&type=single`)
       .then(response => {
-        
+        setProduct(response.data[0]);
       })
   }, [])
 
