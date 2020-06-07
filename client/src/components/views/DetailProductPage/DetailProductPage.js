f167e67812d1691276c51fd900234a2483026f39
morbi <the_morbidus@hotmail.com>
2020-06-06 23:47:54 -0400

    add product order functionality part 1

--- client/src/components/views/DetailProductPage/DetailProductPage.js
+++ client/src/components/views/DetailProductPage/DetailProductPage.js
@@ -2,9 +2,12 @@ import React, { useEffect, useState } from 'react';
 import Axios from 'axios';
 import ProductImage from './Sections/ProductImage';
 import ProductInfo from './Sections/ProductInfo';
+import { addToCart } from '../../../_actions/user_actions';
+import { useDispatch } from 'react-redux';
 
 function DetailProductPage(props) {
-
+  
+  const dispatch = useDispatch();
   const productId = props.match.params.productId;
   const [Product, setProduct] = useState([]);
 
@@ -14,6 +17,10 @@ function DetailProductPage(props) {
         setProduct(response.data[0]);
       })
   }, [])
+
+  const addToCartHandler = (productId) => {
+    dispatch(addToCart());
+  }
 
   return (
     <div className="postPage" style={{ width: '100%', padding: '3rem 4rem' }}>
@@ -28,7 +35,9 @@ function DetailProductPage(props) {
           <ProductImage detail={Product} />
         </Col>
         <Col lg={12} xs={24}>
-          <ProductInfo detail={Product} />
+          <ProductInfo
+          addToCart={addToCartHandler}
+          detail={Product} />
         </Col>
       </Row>
     </div>
