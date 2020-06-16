930f8454a5203b5df22a055160508641e44ee5e3
morbi <the_morbidus@hotmail.com>
2020-06-16 00:05:54 -0400

    History Page rendering part 2

--- client/src/components/views/HistoryPage/HistoryPage.js
+++ client/src/components/views/HistoryPage/HistoryPage.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
 
 function HistoryPage() {
+
+  const [History, setHistory] = useState([]);
+
+  useEffect(() => {
+    Axios.get('api/users/getHistory')
+      .then(response => {
+        if (response.data.success) {
+          setHistory(response.data.history);
+        } else {
+          alert('Failed to get History');
+        }
+      })
+  }, []);
+
   return (
-    <div>
-      HistoryPage
+    <div style={{ width: '80%', margin: '3rem auto' }}>
+      <div style={{ textAlign: 'center' }}>
+        <h1>History</h1>
+      </div>
+      <br />
+      <table>
+        <thead>
+          <tr>
+            
+          </tr>
+        </thead>
+      </table>
     </div>
   )
 }
